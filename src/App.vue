4af72e6a1af92bc5c7d539b0e067d0e2df4228ea
egarciav-dev-fe <egarciav.dev.fe@gmail.com>
2024-09-11 02:36:34 -0300

    (wip): Inicio de proyecto para desafio pokemones

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PkmnList from './components/PkmnList.vue'
+</script>
+
+<template>
+  <PkmnList />
+</template>
+
+<style scoped></style>
---
+++ src/components/PkmnList.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'PkmnList',
+  data() {
+    return {
+      pokemonList: []
+    }
+  },
+  async created() {
+    try {
+      for (let index = 1; index <= 20; index++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${index}`
+        const { data } = await axios.get(url)
+        this.pokemonList.push(data)
+        this.pokemonList.map((pokemon) => ({
+          ...pokemon,
+          isOK: undefined
+        }))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  computed: {
+    contador() {
+      return ''
+    }
+  }
+}
+</script>
